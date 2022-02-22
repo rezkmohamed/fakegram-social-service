083064523b9531fe45dce616bbab71ff29acfc23
Mohamed Rezk <mohamed.rezk@orbyta.it>
2022-02-22 10:44:31 +0100

    implemented postUtils

--- src/main/java/com/fakegram/microservices/social/utils/PostUtils.java
+++ src/main/java/com/fakegram/microservices/social/utils/PostUtils.java
@@ -1,5 +1,6 @@
 package com.fakegram.microservices.social.utils;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import com.fakegram.microservices.social.dto.PostDTO;
@@ -7,27 +8,31 @@ import com.fakegram.microservices.social.entity.Post;
 
 public class PostUtils {
 	public static PostDTO postEntityToDTO(Post post) {
-		/**
-		 * TODO
-		 */
 		PostDTO postDTO = new PostDTO();
+		postDTO.setDate(post.getDate());
+		postDTO.setDescription(post.getDescription());
+		postDTO.setIdPost(post.getIdPost());
+		postDTO.setIdProfile(post.getIdProfile());
+		postDTO.setImg_post(post.getImg_post());
 		
-		
-		
-		return null;
+		return postDTO;
 	}
 	
 	public static List<PostDTO> postEntityToDTO(List<Post> posts) {
-		/**
-		 * TODO
-		 */
-		return null;
+		List<PostDTO> postsDTO = new ArrayList<>();
+		for(Post post : posts) {
+			postsDTO.add(postEntityToDTO(post));
+		}
+		
+		return postsDTO;
 	}
 	
 	public static Post postDTOToEntity(PostDTO postDTO) {
-		/**
-		 * TODO
-		 */
-		return null;		
+		Post post = new Post();
+		post.setDescription(postDTO.getDescription());
+		post.setIdProfile(postDTO.getIdProfile());
+		post.setImg_post(postDTO.getImg_post());		
+		
+		return post;		
 	}
 }
